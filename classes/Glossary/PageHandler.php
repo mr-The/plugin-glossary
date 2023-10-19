596c897cd79e3bc8321f8da4d146473ab0d221b1
d.khranin <fuknyn@yandex.ru>
2023-10-19 17:10:11 +0300

    add option to noIndexTags

--- classes/Glossary/PageHandler.php
+++ classes/Glossary/PageHandler.php
@@ -50,7 +50,7 @@ class PageHandler {
 
   protected function replaceNoIndexContent($html) {
     $noIndexTags = 
-    '<header.*?>.*?</header>|<footer.*?>.*?</footer>|<abbr.*?>.*?</abbr>|<a.*?>.*?</a>|<form.*?>.*?</form>|<script.*?>.*?</script>|<style.*?>.*?</style>|<title.*?>.*?</title>|<h1.*?>.*?</h1>|<!--.*?-->|<button.*?>.*?</button>|<head.*?>.*?</head>|<iframe.*?>.*?</iframe>|<embed.*?>.*?</embed>|<object.*?>.*?</object>|<audio.*?>.*?</audio>|<video.*?>.*?</video>|<source.*?>.*?</source>|<pre.*?>.*?</pre>|<nav.*?>.*?</nav>|<svg.*?>.*?</svg>|<code.*?>.*?</code>|<cite.*?>.*?</cite>|<canvas.*?>.*?</canvas>|<noscript.*?>.*?</noscript>';
+    '<header.*?>.*?</header>|<footer.*?>.*?</footer>|<abbr.*?>.*?</abbr>|<a.*?>.*?</a>|<form.*?>.*?</form>|<script.*?>.*?</script>|<style.*?>.*?</style>|<title.*?>.*?</title>|<h1.*?>.*?</h1>|<!--.*?-->|<button.*?>.*?</button>|<head.*?>.*?</head>|<iframe.*?>.*?</iframe>|<embed.*?>.*?</embed>|<object.*?>.*?</object>|<audio.*?>.*?</audio>|<video.*?>.*?</video>|<source.*?>.*?</source>|<pre.*?>.*?</pre>|<nav.*?>.*?</nav>|<svg.*?>.*?</svg>|<code.*?>.*?</code>|<cite.*?>.*?</cite>|<canvas.*?>.*?</canvas>|<noscript.*?>.*?</noscript>|<option.*?>.*?</option>';
     $withoutNoIndexTags = mb_ereg_replace_callback($noIndexTags, fn($match) => str_repeat('|', strlen($match[0])), $html);
     $onlyText = mb_ereg_replace_callback("<.*?>", fn($match) => str_repeat('|', strlen($match[0])), $withoutNoIndexTags); 
 
